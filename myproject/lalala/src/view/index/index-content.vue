58ee01efc4f2aa9b9c9fe46d49af10968fdcc380
John Doe <johndoe@example.com>
2017-07-22 17:22:44 +0800

    mdf

--- myproject/lalala/src/view/index/index-content.vue
+++ myproject/lalala/src/view/index/index-content.vue
@@ -12,7 +12,10 @@
                 </mt-swipe>
             </mt-tab-container-item>
             <mt-tab-container-item id="content-2">
-                2
+                <input type="radio" name="1" value="a" id="man" class="radio" checked>
+                <label for="man" class="new_radio">男</label>
+                <input type="radio" name="1" value="b" id="women" class="radio">
+                <label for="women" class="new_radio">女</label>
             </mt-tab-container-item>
             <mt-tab-container-item id="content-3">
                 3
@@ -38,6 +41,32 @@
     }
     .swipe-wrap img {
         width: 100%;
+    }
+    /*改变单选框*/
+    .radio {
+        display: none;
+    }
+    .new_radio{
+        width: 40px;
+        height: 20px;
+        display: inline-block;
+        line-height: 20px;
+        cursor: pointer;
+    }
+    .new_radio::before{
+        vertical-align: middle;
+        content: '';
+        width: 20px;
+        height: 20px;
+        display: inline-block;
+        background: url("../../../images/icon_circle-empty.png") no-repeat no-repeat;
+        -webkit-background-size: 20px 20px;
+        background-size: 20px 20px;
+    }
+    .radio:checked + .new_radio::before{
+        background: url("../../../images/icon_circle-slelected.png") no-repeat no-repeat;
+        -webkit-background-size: 20px 20px;
+        background-size: 20px 20px;
     }
 </style>
 <script>
